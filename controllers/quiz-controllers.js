b907a24e08e6215cebaa904b641d45f8f43ed93a
Mohit Kumar <mohitk02082000@gmail.com>
2021-01-05 16:03:25 +0530

    added adding question adding mechanism

--- controllers/quiz-controllers.js
+++ controllers/quiz-controllers.js
@@ -1,5 +1,39 @@
-const addQuestion = (req, res) => {
+const Question = require("./../models/question");
+
+const addQuestion = async (req, res) => {
 	console.log(req.body);
+
+	const {
+		category,
+		difficulty,
+		question,
+		type,
+		correct_answer,
+		incorrect_answer_1,
+		incorrect_answer_2,
+		incorrect_answer_3,
+	} = req.body;
+
+	const createdQuestion = new Question({
+		category,
+		difficulty,
+		question,
+		type,
+		correct_answer,
+		incorrect_answers: [
+			incorrect_answer_1,
+			incorrect_answer_2,
+			incorrect_answer_3,
+		],
+		approved: false,
+	});
+
+	try {
+		await createdQuestion.save();
+	} catch (error) {
+		console.error(error);
+	}
+
 	res.redirect("http://localhost:3000");
 };
 
